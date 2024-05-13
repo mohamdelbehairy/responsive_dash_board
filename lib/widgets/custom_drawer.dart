6f2b35e99f9ffa6364d34b26a3cb75bcc30d2fd7
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-05-13 02:17:37 +0200

    drawer items and display

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/drawer_model_item.dart';
 import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/widgets/drawer_item.dart';
 import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
+  static const List<DrawerItemModel> items = [
+    DrawerItemModel(title: 'Dashboard', image: Assets.imagesDashboard),
+    DrawerItemModel(title: 'My Transaction', image: Assets.imagesMyTransctions),
+    DrawerItemModel(title: 'Statistics', image: Assets.imagesStatistics),
+    DrawerItemModel(title: 'Wallet Account', image: Assets.imagesWalletAccount),
+    DrawerItemModel(title: 'My Investments', image: Assets.imagesMyInvestments),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Column(
+      child: Column(
         children: [
-          UserInfoListTile(
+          const UserInfoListTile(
               image: Assets.imagesAvatar3,
               title: 'Lekan Okeowo',
-              subtitle: 'demo@gmail.com')
+              subtitle: 'demo@gmail.com'),
+          const SizedBox(height: 8),
+          ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: DrawerItem(drawerItemModel: items[index]),
+                );
+              })
         ],
       ),
     );
--- lib/widgets/user_info_list_tile.dart
+++ lib/widgets/user_info_list_tile.dart
@@ -13,12 +13,11 @@ class UserInfoListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: const Color(0xffFAFAFA),
-      elevation: 0,
-      child: ListTile(
-          leading: SvgPicture.asset(image),
-          title: Text(title, style: AppStyles.styleSemiBold16),
-          subtitle: Text(subtitle, style: AppStyles.styleReqular12)),
-    );
+        color: const Color(0xffFAFAFA),
+        elevation: 0,
+        child: ListTile(
+            leading: SvgPicture.asset(image),
+            title: Text(title, style: AppStyles.styleSemiBold16),
+            subtitle: Text(subtitle, style: AppStyles.styleReqular12)));
   }
 }
