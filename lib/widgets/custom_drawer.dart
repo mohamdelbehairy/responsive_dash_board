c8f8ca12f70dfea08886c971f00d918774f6e8e5
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-05-15 02:06:04 +0200

    finish cutom drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/drawer_model_item.dart';
 import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/widgets/active_and_inactive_item.dart';
 import 'package:responsive_dash_board/widgets/drawer_items_list_view.dart';
 import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
 
@@ -17,10 +19,17 @@ class CustomDrawer extends StatelessWidget {
               title: 'Lekan Okeowo',
               subtitle: 'demo@gmail.com'),
           SizedBox(height: 8),
-          DrawerItemListView()
+          DrawerItemListView(),
+          Expanded(child: SizedBox()),
+          InActiveDraweritem(
+              drawerItemModel: DrawerItemModel(
+                  title: 'Setting system', image: Assets.imagesSettings)),
+          InActiveDraweritem(
+              drawerItemModel: DrawerItemModel(
+                  title: 'Logout account', image: Assets.imagesLogout)),
+          SizedBox(height: 48),
         ],
       ),
     );
   }
 }
-
