3d8fa229cab82e008a5115f29f31b71f6036c241
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-06-07 16:30:39 +0200

    my card part two

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/my_card.dart';
 
 import 'all_expensess_and_quick_invoic_section.dart';
 
@@ -13,7 +14,8 @@ class DashBoardDesktopLayout extends StatelessWidget {
       children: [
         Expanded(child: CustomDrawer()),
         SizedBox(width: 32),
-        Expanded(flex: 2, child: AllExpensessAndQuickInvoicSection())
+        Expanded(flex: 2, child: AllExpensessAndQuickInvoicSection()),
+        Expanded(child: MyCard())
       ],
     );
   }
---
+++ lib/widgets/my_card.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_images.dart';
+
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 420 / 215,
+      child: Container(
+        decoration: ShapeDecoration(
+            image: const DecorationImage(
+                image: AssetImage(Assets.imagesCardBackground)),
+            color: const Color(0xFF4EB7F2),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12))),
+      ),
+    );
+  }
+}
