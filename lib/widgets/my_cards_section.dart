bd6a59d8b1ee33ba9c2198904e1dd4b073f2732d
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-06-12 09:14:04 +0200

    my cards section done

--- lib/widgets/my_cards_section.dart
+++ lib/widgets/my_cards_section.dart
@@ -3,21 +3,42 @@ import 'package:responsive_dash_board/utils/app_styles.dart';
 import 'package:responsive_dash_board/widgets/dots_indicator.dart';
 import 'package:responsive_dash_board/widgets/my_cards_page_view.dart';
 
-class MyCardsSection extends StatelessWidget {
+class MyCardsSection extends StatefulWidget {
   const MyCardsSection({super.key});
 
   @override
+  State<MyCardsSection> createState() => _MyCardsSectionState();
+}
+
+class _MyCardsSectionState extends State<MyCardsSection> {
+  late PageController pageController;
+  int currentPageIndex = 0;
+  @override
+  void initState() {
+    pageController = PageController();
+    pageController.addListener(() {
+      currentPageIndex = pageController.page!.round();
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(
-            width: 420,
-            child: Text('My card', style: AppStyles.styleSemiBold20)),
-        SizedBox(height: 20),
-        MyCardsPageView(),
-        SizedBox(height: 20),
-        DotsIndicator()
+        const Text('My card', style: AppStyles.styleSemiBold20),
+        const SizedBox(height: 20),
+        MyCardsPageView(pageController: pageController),
+        const SizedBox(height: 20),
+        DotsIndicator(currentPageIndex: currentPageIndex)
       ],
     );
   }
