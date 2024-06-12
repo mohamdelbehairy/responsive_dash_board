9406a56f9c6693a22bb27849a9c49e86eca248eb
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-06-12 11:00:35 +0200

    transaction history part 1

---
+++ lib/widgets/transction_history.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+
+class TransctionHistory extends StatelessWidget {
+  const TransctionHistory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text('Transaction History', style: AppStyles.styleSemiBold20),
+            Text('See all',
+                style: AppStyles.styleMedium16
+                    .copyWith(color: const Color(0xFF4EB7F2))),
+          ],
+        )
+      ],
+    );
+  }
+}
