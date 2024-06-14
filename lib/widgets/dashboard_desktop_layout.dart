011939b65e9e0de99375a0882bcdf0151ed5aeae
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-06-14 13:37:09 +0200

    my cards and Transaction history section

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/widgets/transction_history.dart';
+import 'package:responsive_dash_board/widgets/my_card_and_transcation_history_section.dart';
 
 import 'all_expensess_and_quick_invoic_section.dart';
 
@@ -16,7 +16,7 @@ class DashBoardDesktopLayout extends StatelessWidget {
         SizedBox(width: 32),
         Expanded(flex: 2, child: AllExpensessAndQuickInvoicSection()),
         SizedBox(width: 24),
-        Expanded(child: TransctionHistory())
+        Expanded(child: MyCardAndTranscationHistorySection())
       ],
     );
   }
--- lib/widgets/transction_history.dart
+++ lib/widgets/transction_history.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
 import 'package:responsive_dash_board/widgets/transction_history_list_view.dart';
 
 import 'transaaction_history_header.dart';
@@ -13,6 +14,8 @@ class TransctionHistory extends StatelessWidget {
       children: [
         TransctionHistoryHeader(),
         SizedBox(height: 20),
+        Text('13 April 2022',style: AppStyles.styleMedium16),
+        SizedBox(height: 16),
         TransctionHistoryListView()
       ],
     );
