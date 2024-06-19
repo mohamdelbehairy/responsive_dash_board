64b374d5b5699e0d0e27f49644c47bd706c1f516
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-06-19 17:41:37 +0200

    dashboard desktop layout done

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 import 'package:responsive_dash_board/widgets/income_section.dart';
+import 'package:responsive_dash_board/widgets/my_card_and_transcation_history_section.dart';
 
 import 'all_expensess_and_quick_invoic_section.dart';
 
@@ -16,7 +17,16 @@ class DashBoardDesktopLayout extends StatelessWidget {
         SizedBox(width: 32),
         Expanded(flex: 2, child: AllExpensessAndQuickInvoicSection()),
         SizedBox(width: 24),
-        Expanded(child: IncomeSection())
+        Expanded(
+          child: Column(
+            children: [
+              SizedBox(height: 40),
+              MyCardAndTranscationHistorySection(),
+              SizedBox(height: 24),
+              Expanded(child: IncomeSection()),
+            ],
+          ),
+        )
       ],
     );
   }
