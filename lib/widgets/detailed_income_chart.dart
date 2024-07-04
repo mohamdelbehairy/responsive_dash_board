955aceec7b2d9be7778f1406a05f0cdcbf930ece
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-07-04 18:33:27 +0300

    detailed income chart part one

--- lib/widgets/detailed_income_chart.dart
+++ lib/widgets/detailed_income_chart.dart
@@ -1,14 +1,14 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-class IncomeChart extends StatefulWidget {
-  const IncomeChart({super.key});
+class DetailedIncomeChart extends StatefulWidget {
+  const DetailedIncomeChart({super.key});
 
   @override
-  State<IncomeChart> createState() => _IncomeChartState();
+  State<DetailedIncomeChart> createState() => _DetailedIncomeChartState();
 }
 
-class _IncomeChartState extends State<IncomeChart> {
+class _DetailedIncomeChartState extends State<DetailedIncomeChart> {
   int activeIndex = -1;
   @override
   Widget build(BuildContext context) {
@@ -30,22 +30,22 @@ class _IncomeChartState extends State<IncomeChart> {
           }),
       sections: [
         PieChartSectionData(
-            showTitle: false,
+            // showTitle: true,
             value: 40,
             radius: activeIndex == 0 ? 60 : 50,
             color: const Color(0xff208BC7)),
         PieChartSectionData(
-            showTitle: false,
+            // showTitle: false,
             value: 25,
             radius: activeIndex == 1 ? 60 : 50,
             color: const Color(0xff4DB7F2)),
         PieChartSectionData(
-            showTitle: false,
+            // showTitle: false,
             value: 20,
             radius: activeIndex == 2 ? 60 : 50,
             color: const Color(0xff064060)),
         PieChartSectionData(
-            showTitle: false,
+            // showTitle: false,
             value: 22,
             radius: activeIndex == 3 ? 60 : 50,
             color: const Color(0xffE2DECD)),
--- lib/widgets/income_section_body.dart
+++ lib/widgets/income_section_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/utils/size_config.dart';
+import 'package:responsive_dash_board/widgets/detailed_income_chart.dart';
 
 import 'income_chart.dart';
 import 'income_details.dart';
@@ -11,7 +12,7 @@ class IncomeSectionBody extends StatelessWidget {
   Widget build(BuildContext context) {
     var width = MediaQuery.sizeOf(context).width;
     return width >= SizeConfig.desktop && width < 1765
-        ? Container()
+        ? const Expanded(child: DetailedIncomeChart())
         : const Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
